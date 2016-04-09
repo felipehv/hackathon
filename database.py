515dddcdae96df5f45f2de7f7ec69450df63541d
tamycova <tcovacevich@uc.cl>
2016-04-09 16:03:34 -0300

    funciones db

--- database.py
+++ database.py
@@ -1,4 +1,40 @@
 import sqlite3
 
 
-conn = sqlite3.connect('example.db')
+class DB:
+
+    def __init__(self, dbname):
+        self.db = sqlite3.connect(dbname)
+        self.c = self.db.cursor()
+
+    def close(self):
+        self.db.commit()
+        self.c.close()
+
+    def add_ingredient(self, userid, ingname):
+        self.c.execute(
+            "INSERT INTO has VALUES ({0}, '{1}')".format(int(userid), ingname))
+
+    def del_ingredient(self, userid, ingname):
+        self.c.execute("DELETE FROM has WHERE userid={0} and ingredient='{1}'".format(
+            int(userid), ingname))
+
+    def match(self, *ids):
+        recetas = []
+        ingredients = []
+        for i in ids:
+            ingredients.append(self.c.execute(
+                "SELECT has.ingredient FROM has WHERE userid={}".format(i)).fetchall())
+        group_ingredients = [i[0][0] for i in ingredients]
+        for j in self.c.execute("SELECT * FROM recipes"):
+            uses = j[3].split(",")
+            filtro = list(filter(lambda x: x not in group_ingredients, uses))
+            if len(filtro) == 0:
+                recetas.append((j[1], j[4]))
+        return recetas
+
+
+
+db = DB("food.db")
+print(db.match(0, 2, 1))
+db.close()
--- poblar.py
+++ poblar.py
@@ -14,7 +14,6 @@ class Generator:
         self.c.execute(
             '''CREATE TABLE users (userid int, username varchar(20), password varchar(20), PRIMARY KEY(userid))''')
 
-
     def test(self):
         self.c.execute("INSERT INTO users VALUES (0,'tamy','t')")
         self.c.execute("INSERT INTO users VALUES (1,'benja','b')")
@@ -22,13 +21,16 @@ class Generator:
         self.c.execute("INSERT INTO has VALUES (0,'tomate')")
         self.c.execute("INSERT INTO has VALUES (1, 'palta')")
         self.c.execute("INSERT INTO has VALUES (2, 'pan')")
-        self.c.execute("INSERT INTO recipes VALUES (0,'Completo Italiano', 3, 'tomate,palta,pan', './pics/0')")
+        self.c.execute(
+            "INSERT INTO recipes VALUES (0,'Completo Italiano', 3, 'tomate,palta,pan', './pics/0')")
+        self.c.execute(
+            "INSERT INTO recipes VALUES (1,'Completo Tomate', 2, 'tomate,pan', './pics/1')")
 
     def close(self):
         self.db.commit()
         self.c.close()
 
-gen = Generator("1.db")
+gen = Generator("food.db")
 gen.create_tables()
 gen.test()
 gen.close()
